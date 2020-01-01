e83bb3093a839dc6a3b3bd9de04865311a52f66e
Adrian Sochaniwsky <adr.soch@outlook.com>
2019-12-31 19:25:15 -0500

    Initialized zombie game

--- zombie/zombie/stdafx.cpp
+++ zombie/zombie/stdafx.cpp
@@ -1,5 +1,5 @@
 // stdafx.cpp : source file that includes just the standard includes
-// SFML_TEMPLATE.pch will be the pre-compiled header
+// zombie.pch will be the pre-compiled header
 // stdafx.obj will contain the pre-compiled type information
 
 #include "stdafx.h"
