8f46104b0cccac3a1376461ceedcf318cc44036b
Adrian Sochaniwsky <adr.soch@outlook.com>
2020-01-04 08:24:19 -0500

    Fixed window not displaying issue

--- zombie/zombie/zombie.cpp
+++ zombie/zombie/zombie.cpp
@@ -33,8 +33,10 @@ int main()
 		// INPUT
 		//###################################
 		Event event;
-		while (window.pollEvent(event)) {
-			if (event.type == Event::KeyPressed) {
+		while (window.pollEvent(event)) 
+		{
+			if (event.type == Event::KeyPressed) 
+			{
 				if (event.key.code == Keyboard::Return &&
 					state == State::PLAYING)
 				{
@@ -51,99 +53,118 @@ int main()
 				{
 					state = State::LEVELING_UP;
 				}
-				if (state == State::PLAYING) 
-				{
-
-				}
-			}
-			// Player Movement %%%%%%%%%%%%%%%%%%%%%%%%%%%%
-			if (Keyboard::isKeyPressed(Keyboard::Escape)){window.close();}
-
-			if (state == State::PLAYING)
-			{
-				if (Keyboard::isKeyPressed(Keyboard::W)) {
-					player.moveUp();
-				}
-				else {player.stopUp();}
-
-				if (Keyboard::isKeyPressed(Keyboard::S)) {
-					player.moveDown();
-				}
-				else { player.stopDown(); }
-
-				if (Keyboard::isKeyPressed(Keyboard::A)) {
-					player.moveLeft();
-				}
-				else { player.stopLeft(); }
-
-				if (Keyboard::isKeyPressed(Keyboard::D)) {
-					player.moveRight();
-				}
-				else { player.stopRight(); }
-			}
-
-			// Level Up %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-			if (state == State::LEVELING_UP)
-			{
-				if (event.key.code == Keyboard::Num1) {
-					state = State::PLAYING;
-				}
-				if (event.key.code == Keyboard::Num2) {
-					state = State::PLAYING;
-				}
-				if (event.key.code == Keyboard::Num3) {
-					state = State::PLAYING;
-				}
-				if (event.key.code == Keyboard::Num4) {
-					state = State::PLAYING;
-				}
-				if (event.key.code == Keyboard::Num5) {
-					state = State::PLAYING;
-				}
-				if (event.key.code == Keyboard::Num6) {
-					state = State::PLAYING;
-				}
-
-				// ################################
-				// Frame Updating
-				//#################################
 				if (state == State::PLAYING)
 				{
-					Time dt = clock.restart();
-					gameTimeTotal += dt;
-
-					float dtSec = dt.asSeconds();
-
-					mouseScreenPosition = Mouse::getPosition();
-					mouseWorldPosition = window.mapPixelToCoords(
-						Mouse::getPosition(), mainView);
-
-					Vector2f playerPosition(player.getCenter());
-					mainView.setCenter(player.getCenter());
-
-				} // End Update FrameS
-
-				// ##################################
-				// Draw Scene
-				// ##################################
-				if (state == State::PLAYING)
-				{
-					window.clear();
-					window.setView(mainView);
-
-					window.draw(player.getSprite());
-				}
-
-				if (state == State::LEVELING_UP) {
-
-				}
-
-				if (state == State::PAUSED) {
 
 				}
 			}
-		} // Evend og Event Polling
+		} // End Events
 
+		// Player Movement %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+		if (Keyboard::isKeyPressed(Keyboard::Escape)){window.close();}
+
+		if (state == State::PLAYING)
+		{
+			if (Keyboard::isKeyPressed(Keyboard::W)) {
+				player.moveUp();
+			}
+			else {player.stopUp();}
+
+			if (Keyboard::isKeyPressed(Keyboard::S)) {
+				player.moveDown();
+			}
+			else { player.stopDown(); }
+
+			if (Keyboard::isKeyPressed(Keyboard::A)) {
+				player.moveLeft();
+			}
+			else { player.stopLeft(); }
+
+			if (Keyboard::isKeyPressed(Keyboard::D)) {
+				player.moveRight();
+			}
+			else { player.stopRight(); }
+		} // End of WASD controls
+
+		// Level Up %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+		if (state == State::LEVELING_UP)
+		{
+			if (event.key.code == Keyboard::Num1) {
+				state = State::PLAYING;
+			}
+			if (event.key.code == Keyboard::Num2) {
+				state = State::PLAYING;
+			}
+			if (event.key.code == Keyboard::Num3) {
+				state = State::PLAYING;
+			}
+			if (event.key.code == Keyboard::Num4) {
+				state = State::PLAYING;
+			}
+			if (event.key.code == Keyboard::Num5) {
+				state = State::PLAYING;
+			}
+			if (event.key.code == Keyboard::Num6) {
+				state = State::PLAYING;
+			}
+
+			if (state == State::PLAYING) {
+				arena.width = 500;
+				arena.height = 500;
+				arena.top = 0;
+				arena.left = 0;
+
+				int tileSize = 50;
+				player.spawn(arena, resolution, tileSize);
+				clock.restart();
+
+			}
+		} // End Level Up
+			
+		// ################################
+		// Frame Updating
+		//#################################
+		if (state == State::PLAYING)
+		{
+			Time dt = clock.restart();
+			gameTimeTotal += dt;
+
+			float dtSec = dt.asSeconds();
+
+			mouseScreenPosition = Mouse::getPosition();
+			mouseWorldPosition = window.mapPixelToCoords(
+				Mouse::getPosition(), mainView);
+
+			player.update(dtSec, Mouse::getPosition());
+			Vector2f playerPosition(player.getCenter());
+			mainView.setCenter(player.getCenter());
+
+		} // End Update Frames
+
+		// ##################################
+		// Draw Scene
+		// ##################################
+		if (state == State::PLAYING)
+		{
+			window.clear();
+			window.setView(mainView);
+
+			window.draw(player.getSprite());
+		}
+
+		if (state == State::LEVELING_UP) {
+
+		}
+
+		if (state == State::PAUSED) {
+
+		}
+
+		if (state == State::GAME_OVER) {
+
+		}
+
+		window.display();
 
 	} //End of while
 
